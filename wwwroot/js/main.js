5defe3a9913b82d35b7d53c9bca37b8dd1e6bb99
Anton <antonhogstrom1994@gmail.com>
2022-01-20 15:34:42 -0300

    first commit

---
+++ wwwroot/js/main.js
@@ -0,0 +1,16 @@
+﻿const indexh1 = document.getElementById("index_h1");
+
+const randomTitle = () => {
+    let random = Math.round(Math.random() * 3);
+
+    let title;
+    if (random === 1) title = "Welcome to my first ASP.NET website";
+    else if (random === 2) title = "Pretty cool first ASP.NET website huh?";
+    else title = "This Title is dynamically generated! COOL!";
+
+
+
+    indexh1.textContent = title;
+}
+
+window.addEventListener("DOMContentLoaded", randomTitle)
